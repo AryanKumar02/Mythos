67a6fef557081004ca6de074465ab4a3aa3f616c
Aryan <147326575+AryanKumar02@users.noreply.github.com>
2025-02-18 01:41:39 +0000

    Authentication (#15)
    * Add Splash page with animations and button component
    
    * Add Terms & Conditions page and completed basic authentication
    
    * Remove debug logging from AuthModal component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,13 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Splash from "./pages/Splash";
+import TermsPage from "./pages/Terms";// Import the Terms & Conditions page
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Splash />} />
+        <Route path="/terms" element={<TermsPage />} /> {/* Added Terms Page */}
       </Routes>
     </Router>
   );
